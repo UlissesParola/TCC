1725945ca33c53f6f09a71e31d1c50303639849e
Ulisses Parola <ulissesmt@gmail.com>
2017-10-03 09:29:07 -0300

    commit inicial

---
+++ Assets/Scripts/LocalPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class LocalPlayerController : MonoBehaviour
+{
+	public float MovimentSpeed = 5f;
+	public float BoostMultiplier = 2f;
+	
+	private Rigidbody2D _playerRB;
+	// Use this for initialization
+	void Start ()
+	{
+		_playerRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Vector3 moviment = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical"), 0 ) * Time.deltaTime * MovimentSpeed;
+		//transform.position += moviment;
+	}
+
+	private void FixedUpdate()
+	{
+		_playerRB.velocity = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical"), 0 ) * MovimentSpeed;
+		Debug.Log(CrossPlatformInputManager.GetAxis("Vertical"));
+		bool isBoost = CrossPlatformInputManager.GetButton("Boost");
+	}
+}
---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+	public GameObject _player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, this.transform.position.z);
+	}
+
+	public void SetLocalPlayer(GameObject localPlayer)
+	{
+		_player = localPlayer;
+	}
+}
